598716a4c0ce4f0c38490df0db8eb7eac99b3629
joe <jonatanreznik12@gmail.com>
2026-07-21 13:56:43 +0300

    Update 2 Python_bot.py
    now opens file path please note you need this file path and

--- Python_bot.py
+++ Python_bot.py
@@ -1,7 +1,23 @@
 from playwright.sync_api import sync_playwright
+#chooses file path to be able to open yt in a custom user
+user =r'C:\playwright_chrome\User Data'
+
 with sync_playwright() as meow:
-    browser = meow.chromium.launch(headless=False , slow_mo=500)
-    page = browser.new_page()
+    #prevents google from messing with the browser opening
+    data = meow.chromium.launch_persistent_context(
+        user,
+        channel="chrome",  
+        headless=False,    
+        args=[
+            "--profile-directory=Profile 26",
+            "--no-first-run",
+            "--no-default-browser-check"
+        ]
+    )
+   
+    
+    page = data.new_page()
+
     page.goto('https://www.youtube.com/')
     gaming =page.get_by_role('link' ,name ='Shorts' )
     gaming.click()
