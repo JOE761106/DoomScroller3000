fe7899e88d18619e04c892a95c5d500edbe32278
joe <jonatanreznik12@gmail.com>
2026-08-02 14:06:49 +0300

    Update 3 Python_bot.py
    added a loop that like that it scrolls short without stopping

--- Python_bot.py
+++ Python_bot.py
@@ -22,7 +22,19 @@ with sync_playwright() as meow:
     gaming =page.get_by_role('link' ,name ='Shorts' )
     gaming.click()
     page.wait_for_timeout(3000) 
-    
+     i =0
+    while i==0:
+
+        page.keyboard.press('ArrowDown')
+        url =page.url
+        url_list.append(url)
+        print(url_list)
+        print(url)
+        if len(url_list)>=2:
+            if url_list[-1]==url_list[-2]:
+                url_list.pop()
+       
+        page.wait_for_timeout(100) 
     
     
     
