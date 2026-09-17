c3206680dbdc87901a56cd527f7f4897031e5bfc
joe <jonatanreznik12@gmail.com>
2026-09-17 18:58:47 +0300

    Add files via upload

--- Python_bot.py
+++ Python_bot.py
@@ -1,9 +1,10 @@
 from playwright.sync_api import sync_playwright
-#chooses file path to be able to open yt in a custom user
+from send_to_discord import Send_to_discord
+
+url_list = []
 user =r'C:\playwright_chrome\User Data'
 
 with sync_playwright() as meow:
-    #prevents google from messing with the browser opening
     data = meow.chromium.launch_persistent_context(
         user,
         channel="chrome",  
@@ -17,12 +18,13 @@ with sync_playwright() as meow:
    
     
     page = data.new_page()
-
     page.goto('https://www.youtube.com/')
-    gaming =page.get_by_role('link' ,name ='Shorts' )
-    gaming.click()
-    page.wait_for_timeout(3000) 
-     i =0
+    
+    shorts= page.get_by_role('link', name='Shorts')
+    shorts.click()
+    
+    page.wait_for_timeout(3000)
+    i =0
     while i==0:
 
         page.keyboard.press('ArrowDown')
@@ -33,8 +35,8 @@ with sync_playwright() as meow:
         if len(url_list)>=2:
             if url_list[-1]==url_list[-2]:
                 url_list.pop()
-       
+        Send_to_discord.content = url
+        Send_to_discord(url)
+        #playlist = "https://www.youtube.com/playlist?list=PLeJk3vJMk3gk"
+
         page.wait_for_timeout(100) 
-    
-    
-    
